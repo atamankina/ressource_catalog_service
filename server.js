b77e97ce8c5f3bb622c889e9a0b1abd085867b02
Galina Atamankina <g.atamankina@gmail.com>
2025-08-04 12:59:09 +0200

    RC-003: Implemented the get /resources/id endpoint.

--- server.js
+++ server.js
@@ -24,6 +24,26 @@ app.get('/resources', (req, res) => {
     }
 });
 
+app.get('/resources/:id', (req, res) => {
+    try {
+        const resourceId = req.params.id;
+        const data = readFileSync(data_file, 'utf8');
+        const resources = JSON.parse(data);
+        const resource = resources.find(r => r.id === resourceId);
+
+        if (resource) {
+            res.json(resource);
+        } else {
+            res.status(404).json({ error: `Ressource mit ID ${resourceId} nicht gefunden.` })
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Daten' });
+    }
+
+
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
